975d0198dcdc32a451b38f1066b9f16f1fbed217
Abdallah <abdallah.mohamed.conan@gmail.com>
2023-10-23 23:01:03 +0300

    don't use cdn

--- sanity/env.ts
+++ sanity/env.ts
@@ -11,7 +11,6 @@ export const projectId = assertValue(
   "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID"
 );
 
-export const useCdn = false;
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
