26af002e0cccf65d34b78df7660a48478057c36e
Abdallah <abdallah.mohamed.conan@gmail.com>
2023-10-25 02:06:04 +0300

    fix error

--- sanity/env.ts
+++ sanity/env.ts
@@ -11,6 +11,7 @@ export const projectId = assertValue(
   "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID"
 );
 
+export const useCdn = true;
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
