e0995b8c26101e18ae22386f46048b7fac550de4
Abdallah <abdallah.mohamed.conan@gmail.com>
2023-12-17 21:22:08 +0200

    update homepage

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,18 +1,13 @@
 import "@/app/globals.css";
 import "@/styles/navbar.css";
 import "@/styles/footer.css";
-import type { Metadata } from "next";
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Belvedere School",
   description: "Belvedere School Cairo",
 };
 
-export default function HomeLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function HomeLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
